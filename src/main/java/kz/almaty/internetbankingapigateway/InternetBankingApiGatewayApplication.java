b8fcc79f382c055a8507b90560632047ff061782
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-30 13:46:35 +0500

    Initial commit

---
+++ src/main/java/kz/almaty/internetbankingapigateway/InternetBankingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package kz.almaty.internetbankingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/almaty/internetbankingapigateway/InternetBankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.almaty.internetbankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
